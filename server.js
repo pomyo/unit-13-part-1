a48413aced60f003cc434278cd37c83a07b20c38
pomyo <yeddev@gmail.com>
2018-12-02 15:21:05 -0500

    added console logs to see what's crashing it

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const mongoose  = require('mongoose');
 const app       = express();                           
 const server    = require('http').createServer(app);   
 const io        = require('socket.io')(server);        
-const PORT      = process.env.PORT || 5000;            
+const PORT      = process.env.PORT || 3000;            
 
 app.use(express.urlencoded({extended: true})); 
 app.use(express.json());
 app.use(express.static('public'));
 
 mongoose.connect('mongodb://heroku_369mh3lg:Rocketscience101!@ds251197.mlab.com:51197/heroku_369mh3lg', { useNewUrlParser : true } );
+console.log(`mongodb://heroku_369mh3lg:Rocketscience101!@ds251197.mlab.com:51197/heroku_369mh3lg`);
 
 require('./sockets/events')(io);     
 require('./routes/html-routes')(app);
